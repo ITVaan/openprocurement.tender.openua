67a424d0950b55b9e6925e1ee4735a7fb439a632
Oleksy <oleksy@artem-iurchenko.unity-bars.com.ua>
2017-07-12 15:16:56 +0300

    merged contracts for open procedures

--- openprocurement/tender/openua/views/contract.py
+++ openprocurement/tender/openua/views/contract.py
@@ -8,6 +8,7 @@ from openprocurement.api.utils import (
     json_view,
     opresource,
     save_tender,
+    check_merged_contracts
 )
 from openprocurement.api.validation import validate_patch_contract_data
 
@@ -36,53 +37,98 @@ class TenderUaAwardContractResource(TenderAwardContractResource):
             self.request.errors.add('body', 'data', 'Can\'t update contract with accepted complaint')
             self.request.errors.status = 403
             return
+
         data = self.request.validated['data']
+        contract = self.request.validated['contract']
 
         if data['value']:
             for ro_attr in ('valueAddedTaxIncluded', 'currency'):
                 if data['value'][ro_attr] != getattr(self.context.value, ro_attr):
                     self.request.errors.add('body', 'data', 'Can\'t update {} for contract value'.format(ro_attr))
                     self.request.errors.status = 403
+
                     return
 
             award = [a for a in tender.awards if a.id == self.request.context.awardID][0]
-            if data['value']['amount'] > award.value.amount:
-                self.request.errors.add('body', 'data', 'Value amount should be less or equal to awarded amount ({})'.format(award.value.amount))
+            max_sum = award.value.amount
+
+            if 'additionalAwardIDs' in contract and contract['additionalAwardIDs']:
+                max_sum += sum(
+                    [award.value.amount for award in tender.awards if award['id'] in contract['additionalAwardIDs']]
+                )
+
+            if data['value']['amount'] > max_sum:
+                self.request.errors.add(
+                    'body', 'data', 'Value amount should be less or equal to awarded amount ({})'.format(max_sum)
+                )
                 self.request.errors.status = 403
+
                 return
 
         if self.request.context.status != 'active' and 'status' in data and data['status'] == 'active':
             award = [a for a in tender.awards if a.id == self.request.context.awardID][0]
             stand_still_end = award.complaintPeriod.endDate
+
             if stand_still_end > get_now():
-                self.request.errors.add('body', 'data', 'Can\'t sign contract before stand-still period end ({})'.format(stand_still_end.isoformat()))
+                self.request.errors.add(
+                    'body', 'data', 'Can\'t sign contract before stand-still period end ({})'.format(
+                        stand_still_end.isoformat()
+                    )
+                )
                 self.request.errors.status = 403
+
                 return
+
             pending_complaints = [
                 i
                 for i in tender.complaints
                 if i.status in tender.block_complaint_status and i.relatedLot in [None, award.lotID]
             ]
+
             pending_awards_complaints = [
                 i
                 for a in tender.awards
                 for i in a.complaints
                 if i.status in tender.block_complaint_status and a.lotID == award.lotID
             ]
+
             if pending_complaints or pending_awards_complaints:
                 self.request.errors.add('body', 'data', 'Can\'t sign contract before reviewing all complaints')
                 self.request.errors.status = 403
+
                 return
+
+            if not self.award_valid(self.request, self.request.context.awardID):
+                return
+
+            for awardID in contract.get('additionalAwardIDs'):
+                if not self.award_valid(self.request, awardID, additional=True):
+                    return
+
+        if check_merged_contracts(self.request) is not None:
+            return
+
         contract_status = self.request.context.status
         apply_patch(self.request, save=False, src=self.request.context.serialize())
-        if contract_status != self.request.context.status and (contract_status != 'pending' or self.request.context.status != 'active'):
+
+        if contract_status != self.request.context.status and (
+                        contract_status != 'pending' or self.request.context.status != 'active'
+        ):
             self.request.errors.add('body', 'data', 'Can\'t update contract status')
             self.request.errors.status = 403
+
             return
+
         if self.request.context.status == 'active' and not self.request.context.dateSigned:
             self.request.context.dateSigned = get_now()
+
         check_tender_status(self.request)
+
         if save_tender(self.request):
-            self.LOGGER.info('Updated tender contract {}'.format(self.request.context.id),
-                        extra=context_unpack(self.request, {'MESSAGE_ID': 'tender_contract_patch'}))
+            self.LOGGER.info(
+                'Updated tender contract {}'.format(self.request.context.id),
+                extra=context_unpack(self.request, {'MESSAGE_ID': 'tender_contract_patch'})
+            )
+
             return {'data': self.request.context.serialize()}
+
